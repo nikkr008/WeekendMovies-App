c16dc293288a7dfc8d54985758ecf6d54cbb61c5
Nikhil Kumar <nik.kr008@gmail.com>
2024-02-25 16:35:45 +0530

    Added language buttons

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,16 +1,29 @@
 import React from 'react'
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity, FlatList} from 'react-native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 export default Logo = () => {
     return (
-        <View style={styles.headingContainer}>
-            <Text  style={styles.headingText}>Movie List</Text>
-            <View style={styles.headerLogo}>
-                <FontAwesome name="user" size={38} color="#ffffff" />
-                <FontAwesome name="heart" size={38} color="#ffffff" />
+        <SafeAreaView>
+            <View style={styles.headingContainer}>
+                <Text  style={styles.headingText}>Movie List</Text>
+                <View style={styles.headerLogo}>
+                    <FontAwesome name="user" size={38} color="#ffffff" paddingRight={20}/>
+                    <FontAwesome name="heart" size={38} color="#ffffff" paddingRight={0}/>
+                </View>
             </View>
-        </View>
+            <View style={styles.languageBtn}>
+                    <TouchableOpacity>
+                        <Text style={styles.languagetxt}>English</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Text style={styles.languagetxt}>Hindi</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Text style={styles.languagetxt}>Tamil</Text>
+                    </TouchableOpacity>
+            </View>
+        </SafeAreaView>
     )
 }
 
@@ -25,12 +38,29 @@ const styles= StyleSheet.create({
         fontSize: 24,
         fontWeight: 'bold',
         color: '#fff',
-        marginRight: 150,
+        marginRight: 120,
     },
     headerLogo:{
         flex:1,
         flexDirection: 'row',
-        justifyContent: 'space-evenly'
+        justifyContent: 'space-evenly', 
     },
-    
+    languageBtn:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        fontSize: 24,
+        fontWeight: 'bold',
+        justifyContent: 'space-evenly',
+        height: 60
+    },
+    languagetxt:{
+        margin: 2,
+        backgroundColor: 'blue',
+        color: 'white',
+        padding: 10,
+        height: 40,
+        width: 100,
+        borderRadius: 5,
+        paddingLeft: 30
+    }
 })
